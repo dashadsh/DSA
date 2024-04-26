8d933d76dd2d0b31b4ce6749a7020d7370b31c13
Daria Goremykina <d.goremykina@gmail.com>
2024-04-26 12:00:08 +0200

    class 88

--- 88/88.cpp
+++ 88/88.cpp
@@ -2,32 +2,37 @@
 
 #include <iostream>
 #include <vector>
-
-void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
-    int i = m - 1;
-    int j = n - 1;
-    int k = m + n - 1;
+class Solution {
+public:
+	void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+   		int i = m - 1;
+   		int j = n - 1;
+    	int k = m + n - 1;
     
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k--] = nums1[i--];
-        } else {
-            nums1[k--] = nums2[j--];
-        }
-    }
+    	while (i >= 0 && j >= 0) {
+        	if (nums1[i] > nums2[j]) {
+            	nums1[k--] = nums1[i--];
+       		} else {
+            	nums1[k--] = nums2[j--];
+        	}
+    	}
     
-    while (j >= 0) {
-        nums1[k--] = nums2[j--];
-    }
-}
+    	while (j >= 0) {
+        	nums1[k--] = nums2[j--];
+    	}
+	}
+};
 
 int main() {
+
+	Solution solution;
+
     std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
     int m = 3;
     std::vector<int> nums2 = {2, 5, 6};
     int n = 3;
     
-    merge(nums1, m, nums2, n);
+    solution.merge(nums1, m, nums2, n);
     
     std::cout << "Merged Array: ";
     for (int i = 0; i < m + n; ++i) {
